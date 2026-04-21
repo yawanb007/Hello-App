9c612e126936629288e8311022d5279c0ebca8f8
yawanb007 <yawanb007@gmail.com>
2026-04-21 14:30:31 +0530

    feature/HelloUC3

---
+++ HelloApp3.java
@@ -0,0 +1,13 @@
+public class HelloApp3 {
+    public static void main(String[] args) {
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
